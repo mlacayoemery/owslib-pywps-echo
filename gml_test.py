18688aeb04924d6d44f001dd1133055325cd5555
Martin Lacayo <mlcayoemery@gmail.com>
2018-06-19 11:40:06 +0200

    Verified that OGR can read OWSLib GML.

---
+++ gml_test.py
@@ -0,0 +1,25 @@
+import owslib.wps
+import lxml.etree
+from gdal import ogr
+
+gml = owslib.wps.GMLMultiPolygonFeatureCollection([[(-102.8184, 39.5273),
+                                                    (-102.8184, 37.418),
+                                                    (-101.2363, 37.418),
+                                                    (-101.2363, 39.5273),
+                                                    (-102.8184, 39.5273)]])
+
+tmp = open("/tmp/gml", 'wb')
+tmp.write(lxml.etree.tostring(gml.getXml()))
+tmp.close()
+        
+drv = ogr.GetDriverByName("GML")
+ds = drv.Open(tmp.name ,0)
+
+lyr = ds.GetLayer()
+
+for f in lyr:
+    print (f.GetGeometryRef())
+
+ds.Destroy()
+
+
